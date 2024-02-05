08c93cd10b15e6ae89021d2b6470aba469ce5abe
Steeve Laberge <slaberge@acm.org>
2024-02-05 14:56:38 -0500

    fixed ps2 converter and finished character rom

--- src/hdl/ps2-ascii/ps2_keyboard_to_ascii.vhd
+++ src/hdl/ps2-ascii/ps2_keyboard_to_ascii.vhd
@@ -180,33 +180,60 @@ BEGIN
               --translate letters (these depend on both shift and caps lock)
               IF((shift_r = '0' AND shift_l = '0' AND caps_lock = '0') OR
                 ((shift_r = '1' OR shift_l = '1') AND caps_lock = '1')) THEN  --letter is lowercase
-                CASE ps2_code IS              
-                  WHEN x"1C" => ascii <= x"61"; --a
-                  WHEN x"32" => ascii <= x"62"; --b
-                  WHEN x"21" => ascii <= x"63"; --c
-                  WHEN x"23" => ascii <= x"64"; --d
-                  WHEN x"24" => ascii <= x"65"; --e
-                  WHEN x"2B" => ascii <= x"66"; --f
-                  WHEN x"34" => ascii <= x"67"; --g
-                  WHEN x"33" => ascii <= x"68"; --h
-                  WHEN x"43" => ascii <= x"69"; --i
-                  WHEN x"3B" => ascii <= x"6A"; --j
-                  WHEN x"42" => ascii <= x"6B"; --k
-                  WHEN x"4B" => ascii <= x"6C"; --l
-                  WHEN x"3A" => ascii <= x"6D"; --m
-                  WHEN x"31" => ascii <= x"6E"; --n
-                  WHEN x"44" => ascii <= x"6F"; --o
-                  WHEN x"4D" => ascii <= x"70"; --p
-                  WHEN x"15" => ascii <= x"71"; --q
-                  WHEN x"2D" => ascii <= x"72"; --r
-                  WHEN x"1B" => ascii <= x"73"; --s
-                  WHEN x"2C" => ascii <= x"74"; --t
-                  WHEN x"3C" => ascii <= x"75"; --u
-                  WHEN x"2A" => ascii <= x"76"; --v
-                  WHEN x"1D" => ascii <= x"77"; --w
-                  WHEN x"22" => ascii <= x"78"; --x
-                  WHEN x"35" => ascii <= x"79"; --y
-                  WHEN x"1A" => ascii <= x"7A"; --z
+                CASE ps2_code IS  
+                    -- modified to force uppercase for Apple 1 
+                  WHEN x"1C" => ascii <= x"41"; --A
+                  WHEN x"32" => ascii <= x"42"; --B
+                  WHEN x"21" => ascii <= x"43"; --C
+                  WHEN x"23" => ascii <= x"44"; --D
+                  WHEN x"24" => ascii <= x"45"; --E
+                  WHEN x"2B" => ascii <= x"46"; --F
+                  WHEN x"34" => ascii <= x"47"; --G
+                  WHEN x"33" => ascii <= x"48"; --H
+                  WHEN x"43" => ascii <= x"49"; --I
+                  WHEN x"3B" => ascii <= x"4A"; --J
+                  WHEN x"42" => ascii <= x"4B"; --K
+                  WHEN x"4B" => ascii <= x"4C"; --L
+                  WHEN x"3A" => ascii <= x"4D"; --M
+                  WHEN x"31" => ascii <= x"4E"; --N
+                  WHEN x"44" => ascii <= x"4F"; --O
+                  WHEN x"4D" => ascii <= x"50"; --P
+                  WHEN x"15" => ascii <= x"51"; --Q
+                  WHEN x"2D" => ascii <= x"52"; --R
+                  WHEN x"1B" => ascii <= x"53"; --S
+                  WHEN x"2C" => ascii <= x"54"; --T
+                  WHEN x"3C" => ascii <= x"55"; --U
+                  WHEN x"2A" => ascii <= x"56"; --V
+                  WHEN x"1D" => ascii <= x"57"; --W
+                  WHEN x"22" => ascii <= x"58"; --X
+                  WHEN x"35" => ascii <= x"59"; --Y
+                  WHEN x"1A" => ascii <= x"5A"; --Z           
+--                  WHEN x"1C" => ascii <= x"61"; --a
+--                  WHEN x"32" => ascii <= x"62"; --b
+--                  WHEN x"21" => ascii <= x"63"; --c
+--                  WHEN x"23" => ascii <= x"64"; --d
+--                  WHEN x"24" => ascii <= x"65"; --e
+--                  WHEN x"2B" => ascii <= x"66"; --f
+--                  WHEN x"34" => ascii <= x"67"; --g
+--                  WHEN x"33" => ascii <= x"68"; --h
+--                  WHEN x"43" => ascii <= x"69"; --i
+--                  WHEN x"3B" => ascii <= x"6A"; --j
+--                  WHEN x"42" => ascii <= x"6B"; --k
+--                  WHEN x"4B" => ascii <= x"6C"; --l
+--                  WHEN x"3A" => ascii <= x"6D"; --m
+--                  WHEN x"31" => ascii <= x"6E"; --n
+--                  WHEN x"44" => ascii <= x"6F"; --o
+--                  WHEN x"4D" => ascii <= x"70"; --p
+--                  WHEN x"15" => ascii <= x"71"; --q
+--                  WHEN x"2D" => ascii <= x"72"; --r
+--                  WHEN x"1B" => ascii <= x"73"; --s
+--                  WHEN x"2C" => ascii <= x"74"; --t
+--                  WHEN x"3C" => ascii <= x"75"; --u
+--                  WHEN x"2A" => ascii <= x"76"; --v
+--                  WHEN x"1D" => ascii <= x"77"; --w
+--                  WHEN x"22" => ascii <= x"78"; --x
+--                  WHEN x"35" => ascii <= x"79"; --y
+--                  WHEN x"1A" => ascii <= x"7A"; --z
                   WHEN OTHERS => NULL;
                 END CASE;
               ELSE                                     --letter is uppercase
